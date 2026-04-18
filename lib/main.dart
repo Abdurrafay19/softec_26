a43d203f7e5549ec4d2f472c2c62c618779aff7b
Abdur Rafay <abdurrafay8709@gmail.com>
2026-04-18 14:19:11 +0500

    Placeholder Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
+// lib/main.dart
+
 import 'package:flutter/material.dart';
-import 'screens/dashboard_screen.dart';
+import 'core/app_theme.dart';
+import 'navigation/screens/main_navigation_screen.dart';
 
 void main() {
-  runApp(const SmeCashFlowApp());
+  runApp(const FiscalArchitectApp());
 }
 
-class SmeCashFlowApp extends StatelessWidget {
-  const SmeCashFlowApp({super.key});
+class FiscalArchitectApp extends StatelessWidget {
+  const FiscalArchitectApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SME Cash Flow',
+      title: 'The Fiscal Architect',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-          brightness: Brightness.light,
-        ),
-      ),
-      home: const DashboardScreen(),
+      theme: AppTheme.lightTheme,
+      home: const MainNavigationScreen(),
     );
   }
 }
